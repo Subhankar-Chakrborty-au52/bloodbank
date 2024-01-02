8d7d9f88acbcc0a791ad658a3a7c50c7faf89092
Subhankar chakrborty <subhankarchakrborty@gmail.com>
2024-01-02 17:10:45 +0530

    script update

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
 // importing dependencies
-const express = require('express');
-require('dotenv').config();
-const cors = require('cors');
-const initdb = require('./config/dbconnect');
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const initdb = require("./config/dbconnect");
 const app = express();
-const path = require('path')
+const path = require("path");
 initdb();
-
 
 // middlewares
 app.use(express.json());
 app.use(cors());
 
 //routes
-app.use('/api/v1/auth' , require('./routes/authroutes'));
-app.use('/api/v1/inventory' , require("./routes/inventoryRoutes"));
-app.use('/api/v1/analytics' , require("./routes/analyticsRoutes"));
-app.use('/api/v1/admin' , require("./routes/adminRoutes"));
+app.use("/api/v1/auth", require("./routes/authroutes"));
+app.use("/api/v1/inventory", require("./routes/inventoryRoutes"));
+app.use("/api/v1/analytics", require("./routes/analyticsRoutes"));
+app.use("/api/v1/admin", require("./routes/adminRoutes"));
 
-// STATIC FOLDER 
-app.use(express.static(path.join(__dirname , './client/build')))
+// STATIC FOLDER
+// app.use(express.static(path.join(__dirname, "./client/build")));
 
 // STATIC ROUTES
 // app.get('*' , function(req, res){
@@ -32,6 +31,8 @@ app.use(express.static(path.join(__dirname , './client/build')))
 
 // Port and Server starting
 const PORT = process.env.PORT;
-app.listen( PORT , ()=>{
-    console.log(`Node server running in ${process.env.DEV_MODE} Mode on Port ${process.env.PORT}`);
-})
+app.listen(PORT, () => {
+  console.log(
+    `Node server running in ${process.env.DEV_MODE} Mode on Port ${process.env.PORT}`
+  );
+});
--- middlewares/adminMiddleware.js
+++ middlewares/adminMiddleware.js
@@ -1,25 +1,24 @@
 const userModel = require("../models/Usermodel");
 
-module.exports = async (req , res , next) => {
-     try {
-        const user = await userModel.findById(req.body.UserId)
+module.exports = async (req, res, next) => {
+  try {
+    const user = await userModel.findById(req.body.UserId);
 
-        // check admin
-        if(user?.role !== "admin"){
-            return res.status(401).send({
-                success : false,
-                message : "Auth Failed"
-            })
-        }
-        else{
-            next();
-        }
-     } catch (error) {
-        console.log(error)
-        return res.status(401).send({
-            success : false,
-            message : "Auth Failed, Admin API",
-            error
-        })
-     }
-}
+    // check admin
+    if (user?.role !== "admin") {
+      return res.status(401).send({
+        success: false,
+        message: "Auth Failed",
+      });
+    } else {
+      next();
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(401).send({
+      success: false,
+      message: "Auth Failed, Admin API",
+      error,
+    });
+  }
+};
